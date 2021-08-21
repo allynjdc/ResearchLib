8fb2e91d464b95618fbf96b3a31188cabd0518b3
AJ Calcaben <calcabenallynjoy@gmail.com>
2021-08-22 00:56:58 +0800

    Add index files

---
+++ pages/index1.php
@@ -0,0 +1,115 @@
+<DOCTYPE! html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-4.0.0-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-4.0.0-dist/css/bootstrap.min.css">
+
+		
+		<script src="../jquery.js"></script>
+		<script src="../script.js"></script>
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="../bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="../css/body_css.css">
+		
+	</head>
+	<body class="bg-light">
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- navbar-inverse -->
+		  	<div class="container-fluid justify-content-end col-md-10 col-md-offset-1">
+			    <div class="navbar-header">
+			      	<a class="navbar-brand " href="#">
+			      		<p class="title_brand_nav"> Division Digital Research Library </p>
+			      	</a>
+			    </div>
+			    <!-- <ul class="nav navbar-nav">
+			      	<li class=""><a href="#">Home</a></li>
+			      	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+			        	<ul class="dropdown-menu">
+			          		<li><a href="#">Page 1-1</a></li>
+			          		<li><a href="#">Page 1-2</a></li>
+			          		<li><a href="#">Page 1-3</a></li>
+			        	</ul>
+			      	</li>
+			      	<li><a href="#">Page 2</a></li>
+			    </ul> -->
+			    <!-- <ul class="nav justify-content-end">
+			    	<li><a href="#">Memorandums</a></li>
+			    	<li><a href="#">Journals & Books</a></li>
+			      	<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+			      	<li><a href="#">Log in</a></li>
+			    </ul> -->
+			    <ul class="nav ">
+				    <li class="nav-item">
+				      	<a class="nav-link" href="#">Memorandum</a>
+				    </li>
+				    <li class="nav-item">
+				      	<a class="nav-link" href="#">Journals & Books</a>
+				    </li>
+				    <li class="nav-item">
+				      	<a class="nav-link" href="#">Help</a>
+				    </li>
+				    <li class="nav-item">
+				      	<a class="nav-link " href="#">Log in</a>
+				    </li>
+  				</ul>
+		  	</div>
+		</nav>
+
+
+		<!-- MAIN TEMPLATE FOR THE CONTENT -->
+		<div class="container-fluid text-center">    
+		  	<div class="row content">
+			    <div class="col-sm-2 sidenav">
+			      	<!-- <p><a href="#">Link</a></p>
+			      	<p><a href="#">Link</a></p>
+			      	<p><a href="#">Link</a></p> -->
+			    </div>
+			    <div class="col-sm-8 center-div"> 
+			    	<br> <br>
+			      	<img class="img-responsive center-block" src="../images/research icon.png" alt="Division Digital Research Library" width="445" height="330"> 
+
+			      	<br>
+
+			      	<div class="text-center title_brand"> 
+						<h1> Division Digital Research Library </h1>
+			      	</div>
+
+			      	<br>
+			      	
+					<div class="col-md-6 col-md-offset-3 form-row align-items-center">
+						<form action="/action_page.php">
+    						<div class="input-group">
+     							<input type="text" class="form-control" placeholder="Search" name="search">
+      							<div class="input-group-btn">
+       								<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search">&nbsp;</i></button>
+      							</div>
+    						</div>
+ 						</form>
+					</div>
+						
+			    </div>
+
+			    <div class="col-sm-2 sidenav">
+			    	<!-- <div class="well">
+			        	<p>ADS</p>
+			      	</div>
+			      	<div class="well">
+			        	<p>ADS</p>
+			      	</div> -->
+			    </div>
+		  </div>
+		</div>
+
+
+		<!-- Footer -->
+		<footer class="container-fluid text-center mt-auto">
+  			<p>all rights reserved.</p>
+		</footer>
+
+	</body>
+</html>
+
+
---
+++ pages/template.php
@@ -0,0 +1,77 @@
+<DOCTYPE! html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+		
+		<script src="../jquery.js"></script>
+		<script src="../script.js"></script>
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="../css/body_css.css">
+		
+	</head>
+	<body class="bg-light">
+		<!-- Navigation -->
+		<nav class="navbar navbar-default"> <!-- navbar-inverse -->
+		  	<div class="container-fluid col-md-10 col-md-offset-1">
+			    <div class="navbar-header">
+			      	<a class="navbar-brand " href="#">
+			      		<p class="title_brand_nav"> Division Digital Research Library </p>
+			      	</a>
+			    </div>
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li><a href="#">Memorandums</a></li>
+			    	<li><a href="#">Journals & Books</a></li>
+			      	<li><a href="#">Log in</a></li>
+			    </ul>
+		  	</div>
+		</nav>
+
+
+		<!-- MAIN TEMPLATE FOR THE CONTENT -->
+		<div class="container-fluid text-center">    
+		  	<div class="row content">
+
+		  		<!-- LEFT SIDE NAVIGATION -->
+			    <div class="col-sm-2 sidenav">
+			      	<p><a href="#">Link</a></p>
+			      	<p><a href="#">Link</a></p>
+			      	<p><a href="#">Link</a></p>
+			    </div>
+
+			    <!-- MIDDLE CONTENT -->
+			    <div class="col-sm-8 center-div"> 
+			    	
+			      	<div class=""> 
+						<h1> Division Digital Research Library </h1>
+			      	</div>
+
+			      	<br>
+						
+			    </div>
+
+			    <!-- RIGHT SIDE NAVIGATION -->
+			    <div class="col-sm-2 sidenav">
+			    	<div class="well">
+			        	<p>ADS</p>
+			      	</div>
+			      	<div class="well">
+			        	<p>ADS</p>
+			      	</div>
+			    </div>
+		  </div>
+		</div>
+
+
+		<!-- Footer -->
+		<footer class="container-fluid text-center mt-auto">
+  			<p>all rights reserved.</p>
+		</footer>
+
+	</body>
+</html>
