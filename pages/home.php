ca9be2626ea289cf2bb09deda90b1dc597e3b06f
OtorSama <jemwelautor@gmail.com>
2021-08-22 22:47:56 +0800

    added default nav and footer to home page

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,64 @@
-<?php
-echo "This is home page.";
-?>
+<DOCTYPE! html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+		
+		<script src="../jquery.js"></script>
+		<script src="../script.js"></script>
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="../css/body_css.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer_css.css">
+        <link rel="stylesheet" type="text/css" href="../css/nav_css.css">
+		
+	</head>
+	<body class="bg-light">
+		<!-- Navigation -->
+		<nav class="navbar navbar-default"> <!-- navbar-inverse -->
+		  	<div class="container-fluid col-md-10 col-md-offset-1">
+			    <div class="navbar-header">
+			      	<a class="navbar-brand " href="#">
+			      		<p class="title_brand_nav"> Division Digital Research Library </p>
+			      	</a>
+			    </div>
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li><a href="#">Memorandums</a></li>
+			    	<li><a href="#">Journals & Books</a></li>
+                    <li><a href="#">Help</a></li>
+			      	<li class="dropdown">
+                        <a href="#" class="dropbtn">MyUser </a>
+                        <div class="dropdown-content">
+                            <a href="user_profile.php">View Profile</a>
+                            <a href="#">Edit Profile</a>
+                            <a href="index.php">Log out</a>
+                        </div>
+                    </li>
+			    </ul>
+		  	</div>
+		</nav>
+
+
+		<!-- MAIN TEMPLATE FOR THE CONTENT -->
+		<div class="container-fluid text-center">    
+		  	<div class="row content">
+
+			    <!-- MIDDLE CONTENT -->
+			    <div class="col-sm-12 center-div"> 
+                    <h3> This is home page. </h3>
+			    </div>
+		  </div>
+		</div>
+
+
+		<!-- Footer -->
+		<footer class="container-fluid text-center mt-auto">
+  			<p>All rights reserved &copy; 2021</p>
+		</footer>
+
+	</body>
+</html>
