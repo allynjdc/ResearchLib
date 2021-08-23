ebe4e9ce0f682abb0bfd3b41b2a561372a514e91
AJ Calcaben <calcabenallynjoy@gmail.com>
2021-08-23 22:37:32 +0800

    all memorandum features

---
+++ pages/memorandum_view.php
@@ -0,0 +1,93 @@
+<DOCTYPE! html>
+<html>
+	<head>
+		<title> Research eLibrary </title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+		<script src="../jquery.js"></script>
+		<script src="../script.js"></script>
+	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" type="text/css" href="../css/body_css.css">
+		<link rel="stylesheet" type="text/css" href="../css/footer_css.css">
+		
+	</head>
+	<body class="bg-light">
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-default"> <!-- navbar-inverse -->
+		  	<div class="container-fluid col-md-10 col-md-offset-1">
+			    <div class="navbar-header">
+			      	<a class="navbar-brand " href="#">
+			      		<p class="title_brand_nav"> Division Digital Research Library </p>
+			      	</a>
+			    </div>
+			    
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li><a href="#">Memorandums</a></li>
+			    	<li><a href="#">Journals & Books</a></li>
+			      	<!-- <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li> -->
+			      	<li><a href="login.php">Log in</a></li>
+			    </ul>
+		  	</div>
+		</nav>
+
+		<!-- MAIN TEMPLATE FOR THE CONTENT -->
+		<div class="container-fluid ">    
+		  	<div class="row content">
+
+			    <div class="col-sm-3 sidenav" >
+			    	<!-- <div class="" style="background-color: white;">
+			      	<p><a href="#">Link</a></p>
+			      	<p><a href="#">Link</a></p>	
+			      	<p><a href="#">Link</a></p>
+			      	</div> -->
+			    </div>
+
+			    <div class="col-sm-6 center_content" style="background-color: white;">
+			    <div class=" center_content" >
+			    		      	
+					<div>
+						<p class="author_name h4" style="color: maroon;"> <b>VIRTUAL TRAINING ON ADVANCING RESEARCH THROUGH 6D SCHEME FOR SECOND BATCH </b> </p>
+					</div>
+
+					<div>
+						<p>May 11, 2021 </p>
+						<p>DM 284, s. 2021 </p>
+						
+					</div>
+					<div class="align-items-center">
+						<iframe src="../resources/memo/DM284-05-12-2021-VIRTUAL-TRAINING-ON-ADVANCING-RESEARCH-THROUGH-6D-SCHEME-FOR-SECOND-BATCH.pdf" width="100%" height="700px">
+    					</iframe>
+    					<br> <br>
+					</div>
+					
+				</div>
+				</div>
+						
+				<div class="col-sm-3 sidenav" >
+			    	<!-- <div style="background-color: white;">
+			    
+				    	<div class="well">
+				        	<p>ADS</p>
+				      	</div>
+				      	<div class="well">
+				        	<p>ADS</p>
+				      	</div>
+				    </div> -->
+			    </div>
+		  </div>
+		</div>
+
+		<!-- Footer -->
+		<!-- <footer class="container-fluid text-center mt-auto">
+  			<p>All rights reserved &copy; 2021</p>
+		</footer> -->
+
+	</body>
+</html>
