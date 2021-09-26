6406f164c3eeb35903a9f9725c68bfdc094cf873
OtorSama <jemwelautor@gmail.com>
2021-09-26 20:51:54 +0800

    login basic functionality done

---
+++ database/authenticate.php
@@ -0,0 +1,17 @@
+<?php
+include 'db_config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $in = file_get_contents('php://input');
+    $decoded = json_decode($in, true);
+    $username = $decoded['uname'];
+    $password = $decoded['pwd'];
+
+    $query = "SELECT * FROM user WHERE username='$username' AND password=MD5('$password')";
+    $result = $db->query($query);
+    $count = mysqli_num_rows($result);
+    if ($count == 1) echo "OK";
+    else echo "NOK";
+}
+?>
