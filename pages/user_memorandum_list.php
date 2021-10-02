bb7418091995f39ff1bc3509adcc40d9bb77c502
AJ Calcaben <calcabenallynjoy@gmail.com>
2021-10-03 00:39:48 +0800

    fixed homepage hyperlinks - II

--- pages/user_memorandum_list.php
+++ pages/user_memorandum_list.php
@@ -2,21 +2,23 @@
 <html>
 	<head>
 		<title> Research eLibrary </title>
-
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
 
+		
 		<script src="../jquery.js"></script>
 		<script src="../script.js"></script>
 	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 
 		<link rel="stylesheet" type="text/css" href="../css/body_css.css">
-		<link rel="stylesheet" type="text/css" href="../css/footer_css.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer_css.css">
+        <link rel="stylesheet" type="text/css" href="../css/nav_css.css">
 		
 	</head>
+
 	<body class="bg-light">
 
 		<!-- Navigation -->
