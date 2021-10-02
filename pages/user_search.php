82b4f50aa0398817659c1c3e29e4026b6cec6bb2
OtorSama <jemwelautor@gmail.com>
2021-10-03 01:34:31 +0800

    add session handling to the rest of the pages

--- pages/user_search.php
+++ pages/user_search.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if (!$_SESSION['user']) {
+	header("Location:index.php");   // Redirect to index page. User cannot view this page if he/she is not yet logged in.
+}
+?>
+
 <DOCTYPE! html>
 	<html>
 		<title> Research eLibrary </title>
@@ -31,12 +39,12 @@
 			    	<li><a href="memorandum.php">Memorandums</a></li>
 			    	<li><a href="journals.php">Journals</a></li>
                     <li><a href="help.php">Help</a></li>
-			      	<li class="dropdown">
-                        <a href="#" class="dropbtn">MyUser </a>
+					<li class="dropdown">
+                        <a href="#" class="dropbtn"><?= $_SESSION['user'] ?></a>
                         <div class="dropdown-content">
                             <a href="user_profile_view.php">View Profile</a>
                             <a href="user_profile_update.php">Edit Profile</a>
-                            <a href="index.php">Log out</a>
+                            <a href="logout.php">Log out</a>
                         </div>
                     </li>
 			    </ul>
